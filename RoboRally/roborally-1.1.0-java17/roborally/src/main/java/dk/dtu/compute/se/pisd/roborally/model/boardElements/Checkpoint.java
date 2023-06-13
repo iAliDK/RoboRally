cb2a81c63a5e981ebcfb0c734f0d5329320de43b
DanielFS211 <s211449@dtu.dk>
2023-06-13 14:44:55 +0200

    Fixed Checkpoint logic

--- RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/boardElements/Checkpoint.java
+++ RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/boardElements/Checkpoint.java
@@ -2,21 +2,31 @@ package dk.dtu.compute.se.pisd.roborally.model.boardElements;
 
 import dk.dtu.compute.se.pisd.roborally.controller.GameController;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
 
-public class Checkpoint extends FieldAction{
-
+public class Checkpoint extends FieldAction {
 
 
     int checkpointNumber;
-    public Checkpoint(int checkpointNumber)  {
-      this.checkpointNumber = checkpointNumber;
+
+    public Checkpoint(int checkpointNumber) {
+        this.checkpointNumber = checkpointNumber;
     }
+
     @Override
     public boolean doAction(GameController gameController, Space space) {
+        Player player = space.getPlayer();
+        if (checkpointNumber == player.getPlayerCounter()) {
+            player.playerCounter++;
+            if (player.playerCounter == 5) {
+                player.setGameWon(true);
+            }
+            return true;
+        }
         return false;
     }
 
     public int getCheckpointNumber(){
          return checkpointNumber;
     }
-}
+    }
